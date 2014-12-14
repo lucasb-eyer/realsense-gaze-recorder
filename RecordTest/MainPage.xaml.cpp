a7a8e18f29aff942890be89967db83307fa8dd59
lucasb-eyer <lucasb.eyer.be@gmail.com>
2014-12-14 14:05:43 +0100

    Untabyfied. Go to TOOLS->Options...->Text Editor->C/C++->Tabs and change to spaces.

--- RecordTest/MainPage.xaml.cpp
+++ RecordTest/MainPage.xaml.cpp
@@ -22,41 +22,41 @@ using namespace Windows::UI::Xaml::Navigation;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 MainPage::MainPage()
-	: _senseManager(PXCSenseManager::CreateInstance())
+    : _senseManager(PXCSenseManager::CreateInstance())
 {
-	InitializeComponent();
+    InitializeComponent();
 
-	if (!_senseManager)
-		throw ref new FailureException("Unable to create the Intel RealSense Manager (PXCSenseManager)");
+    if (!_senseManager)
+        throw ref new FailureException("Unable to create the Intel RealSense Manager (PXCSenseManager)");
 }
 
 MainPage::~MainPage()
 {
-	// Will never be null since we throw in the constructor if it fails.
-	// Also doesn't need to be deleted according to the SDK.
-	_senseManager->Release();
+    // Will never be null since we throw in the constructor if it fails.
+    // Also doesn't need to be deleted according to the SDK.
+    _senseManager->Release();
 }
 
 void RecordTest::MainPage::start(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
-	// Put the UI into "recording" state.
-	_startstopBtn->IsChecked = true;
-	_startstopBtn->Content = "STOP";
+    // Put the UI into "recording" state.
+    _startstopBtn->IsChecked = true;
+    _startstopBtn->Content = "STOP";
 
-	// TODO: 1. Start recording using the RealSense API
+    // TODO: 1. Start recording using the RealSense API
 
-	// 2. Start moving `_datdot` using the "Storyboarded Animation" stuff.
-	_storyboard->Begin();
+    // 2. Start moving `_datdot` using the "Storyboarded Animation" stuff.
+    _storyboard->Begin();
 }
 
 void RecordTest::MainPage::stop(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
-	// Put the UI into "not recording" state.
-	_startstopBtn->IsChecked = false;
-	_startstopBtn->Content = "START";
+    // Put the UI into "not recording" state.
+    _startstopBtn->IsChecked = false;
+    _startstopBtn->Content = "START";
 
-	// TODO: 1. Stop recording and save the file.
+    // TODO: 1. Stop recording and save the file.
 
-	// 2. Stop animating `_datdot` and place it back to its initial position.
-	_storyboard->Stop();
+    // 2. Stop animating `_datdot` and place it back to its initial position.
+    _storyboard->Stop();
 }
--- RecordTest/MainPage.xaml.h
+++ RecordTest/MainPage.xaml.h
@@ -11,19 +11,19 @@
 
 namespace RecordTest
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
-	public ref class MainPage sealed
-	{
-	public:
-		MainPage();
-		virtual ~MainPage();
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public ref class MainPage sealed
+    {
+    public:
+        MainPage();
+        virtual ~MainPage();
 
-	private:
-		void start(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
-		void stop(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+    private:
+        void start(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+        void stop(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
 
-		PXCSenseManager* _senseManager;
-	};
+        PXCSenseManager* _senseManager;
+    };
 }
